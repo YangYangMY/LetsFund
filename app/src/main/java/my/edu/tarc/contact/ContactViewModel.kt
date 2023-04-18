c3b53570e1811176ee3e0115a32b9b8814ae98b7
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-04-18 23:44:44 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/contact/ContactViewModel.kt
@@ -0,0 +1,36 @@
+package my.tarc.mycontact
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class ContactViewModel (application: Application): AndroidViewModel(application) {
+    //LiveData gives us updated contacts when they change
+    var contactList : LiveData<List<Contact>>
+    private val repository: ContactRepository
+    var selectedIndex : Int = -1
+
+    init {
+        val contactDao = ContactDatabase.getDatabase(application).contactDao()
+        repository = ContactRepository(contactDao)
+        contactList = repository.allContacts
+    }
+
+    fun addContact(contact: Contact) = viewModelScope.launch{
+         repository.add(contact)
+    }
+
+    fun updateContact(contact : Contact) = viewModelScope.launch {
+        repository.update(contact)
+    }
+
+    fun deleteContact(contact : Contact) = viewModelScope.launch {
+        repository.delete(contact)
+    }
+
+    fun deleteAll() = viewModelScope.launch{
+        repository.deleteAll()
+    }
+}
