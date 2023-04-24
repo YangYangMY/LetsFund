c427e31972bd5bf3e6cf3f5903ce98f4970761d2
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-04-24 20:50:49 +0800

    Added Jetpack Compose

--- app/src/main/java/my/edu/tarc/letsfund/ui/lender/wallet/WalletFragment.kt
+++ app/src/main/java/my/edu/tarc/letsfund/ui/lender/wallet/WalletFragment.kt
@@ -5,11 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import my.edu.tarc.letsfund.databinding.FragmentWalletBinding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import my.edu.tarc.letsfund.R
 
 class WalletFragment : Fragment() {
+    lateinit var composeView: ComposeView
 
     private var _binding: FragmentWalletBinding? = null
 
@@ -32,11 +56,30 @@ class WalletFragment : Fragment() {
         walletViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
+        //Display Android Material 3 Compose
+        composeView = binding.walletbox
+        composeView.setContent {
+            Card() {
+                Box(Modifier.fillMaxSize()) {
+                    Text(
+                        "Hello World",Modifier.align(Alignment.Center), color = Color.Blue,
+                    )
+                }
+            }
+        }
+
+
         return root
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
