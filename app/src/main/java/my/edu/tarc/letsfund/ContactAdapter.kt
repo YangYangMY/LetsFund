ed8ee761f69b4a67e84b566b7cb1155c76c3ab1f
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-05-04 22:30:23 +0800

    Transaction History Done

--- app/src/main/java/my/edu/tarc/letsfund/ContactAdapter.kt
+++ app/src/main/java/my/edu/tarc/letsfund/ContactAdapter.kt
@@ -5,14 +5,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import my.edu.tarc.letsfund.R
 
 class ContactAdapter (private val recordClickListener: RecordClickListener): RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
     //Cached copy of contacts
     private var contactList = emptyList<Contact>()
 
     class ViewHolder (view: View): RecyclerView.ViewHolder(view) {
-        val textViewName: TextView = view.findViewById(R.id.textViewContactName)
+        val textViewName: TextView = view.findViewById(R.id.textViewMethod)
         val textViewContact: TextView= view.findViewById(R.id.textViewContact)
     }
 
---
+++ app/src/main/java/my/edu/tarc/letsfund/ui/lender/wallet/TransactionAdapter.kt
@@ -0,0 +1,47 @@
+package my.edu.tarc.letsfund.ui.lender.wallet
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import my.edu.tarc.letsfund.Contact
+import my.edu.tarc.letsfund.ContactAdapter
+import my.edu.tarc.letsfund.R
+import my.edu.tarc.letsfund.RecordClickListener
+import my.edu.tarc.letsfund.ui.lender.LenderActivity
+
+// Create an adapter class for the recycler view
+class TransactionAdapter(private val transactionList: List<LenderActivity.PaymentHistory>) :
+    RecyclerView.Adapter<TransactionViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.wallet_record, parent, false)
+        return TransactionViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        val transaction = transactionList[position]
+        holder.dateView.text = transaction.date
+        holder.methodView.text = transaction.method
+        holder.amountView.text = transaction.amount.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return transactionList.size
+    }
+}
+
+class TransactionViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val dateView: TextView = itemView.findViewById(R.id.textViewDate)
+        val methodView: TextView = itemView.findViewById(R.id.textViewMethod)
+        val amountView: TextView = itemView.findViewById(R.id.textViewAmount)
+}
+
+
+
+
+
+
+
