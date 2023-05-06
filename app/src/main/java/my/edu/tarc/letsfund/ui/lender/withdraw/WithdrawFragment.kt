26ec8334ac51a510fd1f84f1df97ff05a339b33d
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-05-07 00:27:09 +0800

    Withdraw in Progress

--- app/src/main/java/my/edu/tarc/letsfund/ui/lender/withdraw/WithdrawFragment.kt
+++ app/src/main/java/my/edu/tarc/letsfund/ui/lender/withdraw/WithdrawFragment.kt
@@ -1,60 +1,78 @@
 package my.edu.tarc.letsfund.ui.lender.withdraw
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.compose.ui.graphics.Color
 import my.edu.tarc.letsfund.R
+import my.edu.tarc.letsfund.databinding.FragmentWithdrawBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WithdrawFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WithdrawFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private var _binding: FragmentWithdrawBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_withdraw, container, false)
-    }
+    ): View {
+        _binding = FragmentWithdrawBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WithdrawFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WithdrawFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.spinnerWithdrawOption.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            @SuppressLint("ResourceAsColor")
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0){
+                    binding.editTextMobileNum.visibility = View.INVISIBLE
+                    binding.textViewMobileNo.visibility = View.INVISIBLE
+                    binding.btnWithdraw1.setTextColor(R.color.black)
+                    binding.btnWithdraw1.setBackgroundColor(R.color.white)
+                    binding.btnWithdraw1.isEnabled = false
+                    binding.btnWithdraw1.isClickable = false
+                }
+                else if (position == 1){
+                    binding.editTextMobileNum.visibility = View.VISIBLE
+                    binding.textViewMobileNo.visibility = View.VISIBLE
+                    binding.btnWithdraw1.isEnabled = true
+                    binding.btnWithdraw1.isClickable = true
+                    binding.btnWithdraw1.setTextColor(R.color.white)
+                    binding.btnWithdraw1.setBackgroundColor(R.color.light_green)
+                }
+                else if (position == 2){
+                    binding.editTextMobileNum.visibility = View.INVISIBLE
+                    binding.textViewMobileNo.visibility = View.INVISIBLE
+                    binding.btnWithdraw1.isEnabled = true
+                    binding.btnWithdraw1.isClickable = true
+                    binding.btnWithdraw1.setTextColor(R.color.white)
+                    binding.btnWithdraw1.setBackgroundColor(R.color.light_green)
                 }
             }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                binding.btnWithdraw1.isActivated = false
+            }
+
+        }
+
+
+
+
+
+        return root
     }
+
 }
